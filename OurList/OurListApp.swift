5071003dd0dc9f2cd4ee6e430f4e2f0c73e5d7e9
Taylor Hartman <taylorjhartman@gmail.com>
2023-10-09 19:48:15 -0700

    Initial project setup

---
+++ OurList/OurListApp.swift
@@ -0,0 +1,17 @@
+//
+//  OurListApp.swift
+//  OurList
+//
+//  Created by Taylor Hartman on 10/9/23.
+//
+
+import SwiftUI
+
+@main
+struct OurListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
