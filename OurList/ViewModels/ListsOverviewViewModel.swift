ce6d4aa9f6a5fe60eef48159ed5e9eae18106f52
Taylor Hartman <taylorjhartman@gmail.com>
2023-12-10 12:22:18 -0800

    Fix layout issue due to bad data management

--- OurList/ViewModels/ListsOverviewViewModel.swift
+++ OurList/ViewModels/ListsOverviewViewModel.swift
@@ -13,7 +13,6 @@ class  ListsOverviewViewModel: ObservableObject {
     let userId: String
     @Published var userName: String = ""
     @Published var listCards = [ListCardModel]()
-    var listCardsTemp = [ListCardModel]()
 
     let db = Firestore.firestore()
     var listIds: [String: String] = [:]
@@ -40,28 +39,41 @@ class  ListsOverviewViewModel: ObservableObject {
                 }
 
                 self.listIds = dataLists
-                self.createListModels()
+                self.createListModels() { cardsStorage in
+                    DispatchQueue.main.async {
+                        self.listCards = cardsStorage
+                    }
+                }
             }
     }
 
     // TODO: Move these calls outside of the init? Is that bad form?
-    private func createListModels() {
+    private func createListModels(_ completion: @escaping ([ListCardModel]) -> Void) {
+        var tracker = 0
+        var listCardsTemp = [ListCardModel]()
+        let listCount = listIds.count
         // Need to get list data info
         listIds.forEach { id in
             let db = Firestore.firestore()
             db.collection("lists")
                 .document(id.key)
-                .getDocument { [weak self] snapshot, error in
-                    guard let data = snapshot?.data(), error == nil, let self
+                .getDocument { snapshot, error in
+                    guard let data = snapshot?.data(), error == nil
                     else { return }
 
                     let title = data["name"] as? String ?? id.value
                     let listId = data["id"] as? String ?? id.key
                     let color = data["color"] as? String ?? ""
-                    self.listCards.append(ListCardModel(
+                    listCardsTemp.append(ListCardModel(
                         id: listId,
                         listTitle: title,
                         color: color))
+
+                    if tracker == listCount - 1 {
+                        completion(listCardsTemp)
+                    }
+
+                    tracker += 1
                 }
         }
     }
