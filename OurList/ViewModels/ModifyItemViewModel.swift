f246abb7c0bac20013acccae01cc3eaa0723d85a
Taylor Hartman <taylorjhartman@gmail.com>
2024-01-12 20:49:58 -0800

    Finish editing list item data

--- OurList/ViewModels/ModifyItemViewModel.swift
+++ OurList/ViewModels/ModifyItemViewModel.swift
@@ -14,7 +14,9 @@ class ModifyItemViewModel: ObservableObject {
     @Published var notes = ""
     @Published var dueDate = Date()
     @Published var showAlert = false
+    var createdDate: TimeInterval?
     let itemIdentifier: String?
+    var isDone: Bool?
 
     var listId: String = ""
 
@@ -36,9 +38,10 @@ class ModifyItemViewModel: ObservableObject {
                 guard let data = snapshot?.data(), error == nil, let self else {
                     return
                 }
-                // this isn't working quite right - keeps just replacing w/ fallback
-                let time = ["dueDate"] as? TimeInterval ?? Date().timeIntervalSince1970
+                let time = data["dueDate"] as? TimeInterval ?? Date().timeIntervalSince1970
                 self.dueDate = Date(timeIntervalSince1970: time)
+                self.createdDate = data["createdDate"] as? TimeInterval
+                self.isDone = data["isDone"] as? Bool ?? false
                 self.notes = data["notes"] as? String ?? ""
                 self.title = data["name"] as? String ?? ""
             }
@@ -53,9 +56,7 @@ class ModifyItemViewModel: ObservableObject {
             case .add:
                 saveNewItem()
             case .edit:
-                // we don't want to overwrite the uuid so will have to write
-                // a modify method or at least adjust how we use the item id
-                return
+                modifyExistingItem()
             case .unknown:
                 return
         }
@@ -82,13 +83,30 @@ class ModifyItemViewModel: ObservableObject {
     }
 
     private func modifyExistingItem() {
+        guard let itemIdentifier else {
+            // error toast?
+            return
+        }
+        let modifiedItem = ListItem(
+            id: itemIdentifier,
+            name: title,
+            dueDate: dueDate.timeIntervalSince1970,
+            createdDate: createdDate ?? Date().timeIntervalSince1970,
+            notes: notes,
+            isDone: isDone ?? false)
 
+        // Save Model
+        let db = Firestore.firestore()
+        db.collection("lists")
+            .document(listId)
+            .collection("listItems")
+            .document(itemIdentifier)
+            .setData(modifiedItem.asDictionary())
     }
 
     var canSave: Bool {
         guard 
-            !title.trimmingCharacters(in: .whitespaces).isEmpty,
-            dueDate >= Date().addingTimeInterval(-86400)
+            !title.trimmingCharacters(in: .whitespaces).isEmpty
         else {
             return false
         }
